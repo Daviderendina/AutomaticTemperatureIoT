48443c3225f862ea4fd3c739b9c78d3b0e35017d
Davide Rendina <daviderendina9@gmail.com>
2021-06-11 17:24:44 +0200

    Implemented deep sleep for sensing station

--- code/sensingStation/MeasureArrayHandler.h
+++ code/sensingStation/MeasureArrayHandler.h
@@ -2,7 +2,7 @@ class MeasureArrayHandler {
 
   public:
     short elementsAdded = 0;
-    float* measureArray;
+    int* measureArray;
     int arraySize;
 
     float lastMean = 0;
@@ -11,41 +11,43 @@ class MeasureArrayHandler {
   
     MeasureArrayHandler(int arraySize){
       this -> arraySize = arraySize;
-      measureArray = new float[arraySize];
+      measureArray = new int[arraySize];
     }
 
-    MeasureArrayHandler(int arraySize, float lastMean){
+    MeasureArrayHandler(int arraySize, int lastMean){
       this -> lastMean = lastMean;
       this -> arraySize = arraySize;
-      measureArray = new float[arraySize];
+      measureArray = new int[arraySize];
     }
 
-    float getAverage(){
+    int getAverage(){
       if(elementsAdded == 0)
         return lastMean;
         
-      float sum = 0;
+      int sum = 0;
       for(int i=0; i<arraySize; i++)
         sum += measureArray[i];
-      return sum / elementsAdded;
+      return ((int) sum / elementsAdded);
     }
 
-    void addElement(float newValue){
+    void addElement(int newValue){
       for(int i=0; i<arraySize - 1; i++){
         measureArray[i] = measureArray[i + 1];
       }
       measureArray[arraySize - 1] = newValue;
 
-      if(elementsAdded < 6)
+      if(elementsAdded < arraySize)
         elementsAdded++;
     }
 
     void printArray(){
+      Serial.print("[");
+        Serial.print(F(" "));
       for(int i=arraySize - elementsAdded; i < arraySize; i++){
         Serial.print(measureArray[i]);
         Serial.print(F(" "));
       }
-      Serial.println(F(""));
+      Serial.println(F("]"));
     }
 
     void clearMeasures(){
--- code/sensingStation/sensingStation.ino
+++ code/sensingStation/sensingStation.ino
@@ -1,3 +1,4 @@
+#define MEMORY_DEBUG 0
 
 #include "secrets.h"            
 
@@ -25,7 +26,7 @@
 #define BUTTON_DEBOUNCE_DELAY 20  // Button debounce
 
 // Software constants
-#define SIZE_MEASURES_AVG_ARRAY    6         // Store last 6 values measured
+#define SIZE_MEASURES_AVG_ARRAY    3         // Store last 3 values measured
 
 
 // Initialize objects for sensors
@@ -62,8 +63,7 @@ MeasureArrayHandler humidityMeasures(SIZE_MEASURES_AVG_ARRAY);
 MeasureArrayHandler rssiMeasures(SIZE_MEASURES_AVG_ARRAY);
 float heatIndex;
 String macAddr;
-
-
+int loopCounter;
 
 
 void setup() {
@@ -74,9 +74,16 @@ void setup() {
 
   // Inizialize EPROM
   short eeprom_init = 0;
-  int eprom_size = 3 * 40 + sizeof(eeprom_init) + 2;
+  int single_array_measure_size = sizeof(float) * (3 + SIZE_MEASURES_AVG_ARRAY);
+  int eprom_size = 3 * single_array_measure_size + sizeof(loopCounter) + sizeof(eeprom_init);
+  Serial.print(F("Initializing EEPROM with size: "));
+  Serial.println(eprom_size);
   EEPROM.begin(eprom_size);
   delay(100);
+
+  int temp_addr = sizeof(loopCounter) + sizeof(eeprom_init);
+  int humid_addr = temp_addr + single_array_measure_size;
+  int rssi_addr = humid_addr + single_array_measure_size;
 
   // Connessione Wifi
   connectWifi();
@@ -108,9 +115,7 @@ void setup() {
       break;
   }
 
-  int temp_addr = sizeof(eeprom_init)+2;
-  int humid_addr = temp_addr + 40;
-  int rssi_addr = humid_addr + 40;
+
   
   // Leggo dalla EPROM
   eeprom_init = EEPROM.read(0);
@@ -118,39 +123,44 @@ void setup() {
   Serial.println(eeprom_init);
   if(eeprom_init != 1){
     Serial.println("Nothing has been initialized yet");
+    loopCounter = 0;
   } else {
     temperatureMeasures = readMeasureArray(temp_addr);
     humidityMeasures = readMeasureArray(humid_addr);
     rssiMeasures = readMeasureArray(rssi_addr);
+    loopCounter = EEPROM.read(4) + 1;
   }
-
-  // TEST
-  temperatureMeasures.printArray();
-  humidityMeasures.printArray();
-  rssiMeasures.printArray();
   
   // Leggo i valori dai sensori e aggiorno gli oggetti
   Serial.println("Reading values from sensors");
   readSensors();
 
-  
+  Serial.print("Temperature: ");
   temperatureMeasures.printArray();
+  Serial.print("Humidity: ");
   humidityMeasures.printArray();
+  Serial.print("RSSI: ");
   rssiMeasures.printArray();
   
   // Lo scrivo sulla EPROM
-  Serial.println("Writing measures on EEPROM");
+  Serial.println(F("Writing measures on EEPROM"));
   saveMeasureArray(temperatureMeasures, temp_addr);
   saveMeasureArray(humidityMeasures, humid_addr);
   saveMeasureArray(rssiMeasures, rssi_addr);
   if(eeprom_init != 1){
     EEPROM.write(0, 1);
     EEPROM.commit();
+    delay(300);
   }
-  delay(200);
 
-  // Ogni 3x loop, loggo sul DB la media (contatore anche qui)
-  communicateValues();
+  // Controllo
+  Serial.println(loopCounter);
+  if(loopCounter == 3)
+    communicateValues();
+
+  EEPROM.write(4, loopCounter % 3);
+  EEPROM.commit();
+  delay(300);
 
   // Sleep di 10 minuti
   Serial.println("Going deep sleep for 10 minutes");
@@ -246,30 +256,38 @@ void connectWifi(){
 
 MeasureArrayHandler readMeasureArray(int mem_addr){
   int arraySize = EEPROM.read(mem_addr);
-  Serial.print(mem_addr);
-  Serial.print(" <- ");
-  Serial.println(arraySize);
+  if(MEMORY_DEBUG == 1){
+    Serial.print(mem_addr);
+    Serial.print(" <- ");
+    Serial.println(arraySize);
+  }
   mem_addr += 4;
   
   int elementsAdded = EEPROM.read(mem_addr);
-  Serial.print(mem_addr);
-  Serial.print(" <- ");
-  Serial.println(elementsAdded);
+  if(MEMORY_DEBUG == 1){
+    Serial.print(mem_addr);
+    Serial.print(" <- ");
+    Serial.println(elementsAdded);
+  }
   mem_addr += 4;
 
   float lastMean = EEPROM.read(mem_addr);
-  Serial.print(mem_addr);
-  Serial.print(" <- ");
-  Serial.println(lastMean);
+  if(MEMORY_DEBUG == 1){
+    Serial.print(mem_addr);
+    Serial.print(" <- ");
+    Serial.println(lastMean);
+  }
   mem_addr += 4;
 
   MeasureArrayHandler mh(arraySize, lastMean);
 
   for(int i=arraySize - elementsAdded; i< arraySize; i++){
     float measure = EEPROM.read(mem_addr) -100;
-  Serial.print(mem_addr);
-  Serial.print(" <- ");
-  Serial.println(measure);
+    if(MEMORY_DEBUG == 1){
+      Serial.print(mem_addr);
+      Serial.print(" <- ");
+      Serial.println(measure);
+    }
     mh.addElement(measure);
     mem_addr += 4;
   }
@@ -279,32 +297,40 @@ MeasureArrayHandler readMeasureArray(int mem_addr){
 
 int saveMeasureArray(MeasureArrayHandler handler, int mem_addr){
   EEPROM.write(mem_addr, handler.arraySize);
-  Serial.print(mem_addr);
-  Serial.print(" -> ");
-  Serial.println(handler.arraySize);
+  if(MEMORY_DEBUG == 1){
+    Serial.print(mem_addr);
+    Serial.print(" -> ");
+    Serial.println(handler.arraySize);
+  }
   
   mem_addr += 4;
   EEPROM.write(mem_addr, handler.elementsAdded);
-  Serial.print(mem_addr);
-  Serial.print(" -> ");
-  Serial.println(handler.elementsAdded);
+  if(MEMORY_DEBUG == 1){
+    Serial.print(mem_addr);
+    Serial.print(" -> ");
+    Serial.println(handler.elementsAdded);
+  }
   
   mem_addr += 4;
   EEPROM.write(mem_addr, handler.lastMean);
-  Serial.print(mem_addr);
-  Serial.print(" -> ");
-  Serial.println(handler.lastMean);
+  if(MEMORY_DEBUG == 1){
+    Serial.print(mem_addr);
+    Serial.print(" -> ");
+    Serial.println(handler.lastMean);
+  }
   
   mem_addr += 4;
   for(int i=handler.arraySize - handler.elementsAdded; i< handler.arraySize; i++){
-    Serial.print(mem_addr);
-    Serial.print(" -> ");
-    Serial.println(handler.measureArray[i]+100);
+    if(MEMORY_DEBUG == 1){
+      Serial.print(mem_addr);
+      Serial.print(" -> ");
+      Serial.println(handler.measureArray[i]+100);
+    }
     EEPROM.write(mem_addr, handler.measureArray[i]+100);
     mem_addr += 4;
   }
   EEPROM.commit();
-  delay(200);
+  delay(300);
 
   return mem_addr;
 }
